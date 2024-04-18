0b8c593da8df6935e2fdacd531d28b87a2650c4e
faom002 <faisal_jobb@outlook.com>
2024-04-18 19:47:48 +0200

    cleaned up rectangleboy

--- EscapeGame/main.cpp
+++ EscapeGame/main.cpp
@@ -1,7 +1,8 @@
 #include <complex.h>
-#include <iostream>
-#include <ostream>
 #include <raylib.h>
+
+
+namespace Game {
 
 struct DrawTextComponent {
 
@@ -16,9 +17,12 @@ struct DrawTextComponent {
 struct RectangleBoyComponent {
 
   // position
-  float imag;
-  float real;
-
+  float imag = 0;
+  float real = 0;
+  
+  
+Rectangle player = {imag/2,real/2, 20 ,20};
+  
   // eyesLeftSide
   float eyesLeftX = 2;
   float eyesLeftY = 7;
@@ -31,11 +35,12 @@ struct RectangleBoyComponent {
 
   void Draw() {
 
-    DrawRectangle(imag / 2, real / 2, 20, 20, Color(RED));
+	   
+    DrawRectangleRec(player,Color(RED));
 
-    DrawRectangle(imag / 2 + eyesLeftX, real / 2 + eyesLeftY, 20 - 15, 20 - 13,
+    DrawRectangle(player.x  + eyesLeftX, player.y  + eyesLeftY, 20 - 15, 20 - 13,
                   Color(BLACK));
-    DrawRectangle(imag / 2 + eyesRightX, real / 2 + eyesRightY, 20 - 15,
+    DrawRectangle(player.x + eyesRightX, player.y + eyesRightY, 20 - 15,
                   20 - 13, Color(BLACK));
 
     Update();
@@ -44,11 +49,11 @@ struct RectangleBoyComponent {
   void Update() {
 
     if (IsKeyDown(KEY_LEFT)) {
-      imag -= 100 * GetFrameTime();
+      player.x -= 100 * GetFrameTime();
     } else if (IsKeyDown(KEY_RIGHT)) {
-      imag += 100 * GetFrameTime();
+      player.x += 100 * GetFrameTime();
     } else if (IsKeyDown(KEY_UP)) {
-      real -= 100 * GetFrameTime();
+      player.y -= 100 * GetFrameTime();
 
       eyesLeftY = -3;
 
@@ -56,7 +61,7 @@ struct RectangleBoyComponent {
 
     } else if (IsKeyDown(KEY_DOWN)) {
 
-      real += 100 * GetFrameTime();
+      player.y += 100 * GetFrameTime();
 
       eyesLeftY = 14;
 
@@ -69,12 +74,15 @@ struct RectangleBoyComponent {
     }
   }
 };
-
+}
 int main() {
 
-  auto text = DrawTextComponent(0, 0);
 
-  auto rectanglePlayer = RectangleBoyComponent(800, 500);
+
+
+  auto text = Game::DrawTextComponent(0, 0);
+
+  auto rectanglePlayer = Game::RectangleBoyComponent(800, 500);
 
   InitWindow(800, 500, "rectangle");
 
